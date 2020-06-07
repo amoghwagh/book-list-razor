e73fff1c5bb8614990d7209eb1fb98575a88c22e
Amogh Wagh <amoghwagh@gmail.com>
2020-06-07 17:42:10 +0530

    Added gitignore and added edit and delete to book list page

--- BookListRazor/Pages/BookList/index.cshtml.cs
+++ BookListRazor/Pages/BookList/index.cshtml.cs
@@ -9,13 +9,13 @@ namespace BookListRazor.Pages.BookListRazor
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        public IEnumerable<Book> Books { get; set; }
 
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public IEnumerable<Book> Books { get; set; }
         public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
